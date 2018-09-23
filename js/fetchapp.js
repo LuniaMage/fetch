b255a12cf6a06e7616a605190b85d60650489765
Lunia Mage <luniamage@gmail.com>
2018-09-23 17:08:02 -0400

    draft of search form and setup of fetch api

---
+++ js/fetchapp.js
@@ -0,0 +1,42 @@
+// http://api.petfinder.com/my.method?key=12345&arg1=foo
+
+// Promise.all([ fetchData('http://api.petfinder.com/breed.list?key=de0b855a21fffe3092ffa55ef2dfb18c.json')
+// ]);
+
+
+// function fetchData(url) {
+//   return fetch(url)
+//   .then(checkStatus) 
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+// }
+
+const select = document.querySelector('#breeds');
+const card = document.querySelector('.card');
+const form = document.querySelector('form');
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+
+
+
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+
+
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+
+
+
+// ------------------------------------------
+//  POST DATA
+// ------------------------------------------
