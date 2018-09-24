42e6f2f4279d1f1542e074da4ea8dc263c7ccef4
Lunia Mage <luniamage@gmail.com>
2018-09-23 22:30:37 -0400

    update api fetch. cannot fix 'Access-Control-Allow-Origin' error

--- js/fetchapp.js
+++ js/fetchapp.js
@@ -21,8 +21,24 @@ const form = document.querySelector('form');
 
 
 
+function fetchData(url) {
+    const configGet = {
+      method: GET /breed.list/ HTTP/1.1
+      host: api.petfinder.com,
+      x-api-key: de0b855a21fffe3092ffa55ef2dfb18c,
+      authorization: 3bce7822214549c5cb517ff8baccfb8b,
+      Content-Type: application/json,
+      Access-Control-Allow-Origin: 'api.petfinder.com',
+      Cache-Control: no-cache,
+      Postman-Token: 28064c77-f09d-4ce4-9061-f534680bf6d1
+    };
+      
+      return fetch(url,configGet)
+        .then(response => console.log(response));
+  }
 
-
+   
+// }
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
