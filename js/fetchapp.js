2e8f26e88216de69d2da77d5c18a215e0fcdc27c
Lunia Mage <luniamage@gmail.com>
2018-09-25 01:58:06 -0400

    fix issue with petfinder api by replacing it with dog CEO api from treehouse project

--- js/fetchapp.js
+++ js/fetchapp.js
@@ -4,13 +4,6 @@
 // ]);
 
 
-// function fetchData(url) {
-//   return fetch(url)
-//   .then(checkStatus) 
-//   .then(res => res.json())
-//   .then(data => console.log(data));
-// }
-
 const select = document.querySelector('#breeds');
 const card = document.querySelector('.card');
 const form = document.querySelector('form');
@@ -19,26 +12,36 @@ const form = document.querySelector('form');
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
+const xhr = new XMLHttpRequest();
 
-
-function fetchData(url) {
-    const configGet = {
-      method: GET /breed.list/ HTTP/1.1
-      host: api.petfinder.com,
-      x-api-key: de0b855a21fffe3092ffa55ef2dfb18c,
-      authorization: 3bce7822214549c5cb517ff8baccfb8b,
-      Content-Type: application/json,
-      Access-Control-Allow-Origin: 'api.petfinder.com',
-      Cache-Control: no-cache,
-      Postman-Token: 28064c77-f09d-4ce4-9061-f534680bf6d1
-    };
-      
-      return fetch(url,configGet)
-        .then(response => console.log(response));
+xhr.onreadystatechange = function(response) {
+  if (xhr.readyState === 4 && 200) {
+    document.getElementById('ajax').innerHTML = xhr.responseText;
   }
+}
 
-   
-// }
+xhr.open('GET','http://api.petfinder.com/breed.list?key=de0b855a21fffe3092ffa55ef2dfb18c.jsonp');
+$.ajax({
+  url: 'http://api.petfinder.com/breed.list?format=json&key=de0b855a21fffe3092ffa55ef2dfb18c&animal="dog"',
+  headers: {
+      'URL': 'http://api.petfinder.com',
+      'Access-Control-Allow-Origin': 'http://api.petfinder.com',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept',
+      'Authorization': '3bce7822214549c5cb517ff8baccfb8b',
+      'Cache-Control': 'no-cache',
+      'Postman-Token': 'c1972bc8-4159-49b8-aefe-9e5caeb09a56',
+      'Content-Type':'application/jsonp'
+  },
+  method: 'GET',
+  dataType: 'jsonp'
+});
+xhr.send();
+
+
+
+
+
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
@@ -56,3 +59,13 @@ function fetchData(url) {
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
+
+/*
+GET: /breed.list?format=json&amp;key=de0b855a21fffe3092ffa55ef2dfb18c&amp;animal=dog HTTP/1.1
+Content-Type: application/x-www-form-urlencoded
+Host: api.petfinder.com
+X-Amz-Date: 20180925T001401Z
+Authorization: AWS4-HMAC-SHA256 Credential=/20180925/us-east-1/execute-api/aws4_request, SignedHeaders=access-control-allow-origin;cache-control;content-type;host;postman-token;x-amz-date, Signature=759ff4469053ffba12ca89347c4dcb7011eeece3a905cb49f0b1e2c1ec9b1d82
+Cache-Control: no-cache
+Postman-Token: 7bef8098-2b8f-4aee-ad47-8395d2878fb5
+*/
